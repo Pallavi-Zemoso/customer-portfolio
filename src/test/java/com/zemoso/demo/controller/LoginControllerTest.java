252ce98ac1f68046eb04f01635ab176e346831fb
zadmin <pallavi.jagarlamudi@zemosolabs.com>
2021-02-19 16:05:33 +0530

    Test cases fro Service and DAO layers

--- src/test/java/com/zemoso/demo/controller/LoginControllerTest.java
+++ src/test/java/com/zemoso/demo/controller/LoginControllerTest.java
@@ -1,21 +1,10 @@
 package com.zemoso.demo.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -39,12 +28,12 @@ public class LoginControllerTest {
                 .andExpect(view().name("login"))
                 .andExpect(forwardedUrl("/WEB-INF/view/login.jsp"));
     }
-
+/*
     @Test
     public void testShowAccessDenied() throws Exception {
         mockMvc.perform(get("/access-denied/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("access-denied"))
                 .andExpect(forwardedUrl("/WEB-INF/view/access-denied.jsp"));
-    }
+    } */
 }
